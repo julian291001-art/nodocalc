d1af6f6834b9117190890ffd4a3d5fc18aecb847
Julian Esteban Leon Florez <julianestebanleonflorez@MacBook-Neo-de-Julian.local>
2026-06-23 18:06:33 -0500

    middleware simplificado sin clerk

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+export function middleware() {
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jte|ttf|woff2?|png|jpg|jpeg|gif|svg|ico|webp)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jte|ttf|woff2?|png|jpg|jpeg|gif|svg|ico|webp)).*)", "/(api|trpc)(.*)"],
 };
