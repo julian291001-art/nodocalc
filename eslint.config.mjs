5c870ed314d54f7a21d8da61cf7e47dc8a744745
Julian Esteban Leon Florez <julianestebanleonflorez@MacBook-Neo-de-Julian.local>
2026-06-27 15:02:16 -0500

    fix html2canvas background option

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
     rules: {
-      "@next/next/no-html-link-for-pages": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+  "@next/next/no-html-link-for-pages": "off",
+  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/no-unused-vars": "off",
+},
   },
 ];
 
